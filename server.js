ed819478fe446ce8dcbf81e2185b556ebaf1ba86
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-12-13 13:39:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,3 +91,40 @@ connect('mongodb+srv://saitarakaram:L2HwgE8VLais1o5l@cluster0.opravw6.mongodb.ne
 }).catch((error) => {
     console.log(error)
 })
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000; // You can change the port number as needed
+
+const uri = 'mongodb+srv://saitarakaram:L2HwgE8VLais1o5l@cluster0.opravw6.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection string
+
+app.get('/api/products', async (req, res) => {
+  try {
+    // Create a new MongoClient
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Connect to the MongoDB server
+    await client.connect();
+
+    // Reference to the MongoDB collection where your products are stored
+    const productsCollection = client.db('Node-API').collection('Product');
+
+    // Fetch all products from the collection
+    const products = await productsCollection.find({}).toArray();
+
+    // Respond with the products as JSON
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'An error occurred while fetching products' });
+  } finally {
+    // Close the connection when done
+    client.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
